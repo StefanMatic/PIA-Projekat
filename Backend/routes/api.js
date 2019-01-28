b878cabfac00a73bd68bb359e51ddcb380e51826
StefanMatic <maticstefan55@gmail.com>
2019-01-28 02:46:27 +0100

    Backend
    Prva verzija backenda. Treba da se dorade linkovi za oradu zahteva. Trenutno modeli su samo modeli koji se koriste kao primer

---
+++ Backend/routes/api.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+
+const mongoose = require('mongoose')
+const db = "mongodb://localhost:27017/jobfair"
+
+const User = require('../models/user')
+
+mongoose.connect(db, err =>{
+    if (err){
+        console.log(err)
+    }
+    else{
+        console.log("Connection to database successful")
+    }
+})
+router.get('/', (req, res)=>{
+    res.send("From Api router")
+})
+
+router.post('register', (req, res)=>{
+    let userData = req.body
+    let user = new User(userData)
+
+    user.save((err, user)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.status(200).send(user)
+        }
+    })
+})
+
+router.post('login', (req, res)=>{
+    let userData = req.body
+
+    User.findOne({email: userData.email},(err, user)=>{
+        if (err){
+            console.log(err)
+        }
+        else{
+            if (!user){
+                res.status(401).send("Invalid user")
+            }
+            else {
+                if (user.password !== userData.password){
+                    res.status(401).send("Invalid password")
+                }
+                res.status(200).send(user)
+            }
+        }
+    })
+})
+
+module.exports = router
