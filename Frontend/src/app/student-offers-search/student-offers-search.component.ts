0f27accc37dbef6d98a9304f52c8e5cc28a2e46b
StefanMatic <maticstefan55@gmail.com>
2019-02-05 16:39:59 +0100

    Pretraga ponuda
    Dodata je mogucnost pretrage ponuda

--- Frontend/src/app/student-offers-search/student-offers-search.component.ts
+++ Frontend/src/app/student-offers-search/student-offers-search.component.ts
@@ -9,25 +9,101 @@ import { Router } from '@angular/router';
   styleUrls: ['./student-offers-search.component.css']
 })
 export class StudentOffersSearchComponent implements OnInit {
-  allOffers:Array<Offer> = [];
+  allOffers: Array<Offer> = [];
+  startAllOffers: Array<Offer> = [];
+
+  //promenljive za rad sa pretragom
+  helperArray:Array<Offer> = [];
+  companySearch: String = '';
+  offerSearch: String = '';
+  isJob: Boolean = false;
+  isInternship: Boolean = false;
+  helper: Boolean = false;
 
   constructor(private offerService: CompanyOfferService,
     private router: Router) { }
 
   ngOnInit() {
     this.offerService.getAllOffers().subscribe(
-      (res:Array<Offer>)=>{
+      (res: Array<Offer>) => {
         console.log(res)
+        this.startAllOffers = res
         this.allOffers = res
+        this.helperArray = []
+        this.checkOffersDate()
       },
-      (err)=>console.log(err)
+      (err) => console.log(err)
     )
   }
 
-  openDetails(info:String){
+  checkOffersDate() {
+    console.log(this.allOffers)
+    this.helperArray = this.allOffers;
+    console.log(this.helperArray)
+    for (let offer of this.helperArray) {
+      if (Date.parse(offer.deadlineDate.toString()) > Date.now())
+        this.allOffers.push(offer)
+    }
+  }
+
+  openDetails(info: String) {
     console.log(info)
     localStorage.setItem("offer", info as string)
     this.router.navigate(['/offerDetails'])
+  }
+
+  onReset() {
+    this.allOffers = this.startAllOffers;
+    this.checkOffersDate();
+    this.offerSearch = '';
+    this.companySearch = '';
+    this.isInternship = false;
+    this.isJob = false;
+
+    this.helper = false;
+    this.helperArray = [];
+  }
+
+  onSearch() {
+    this.allOffers = [];
+    this.helper = false;
+
+    for (let offer of this.startAllOffers) {
+      if ((this.offerSearch === '' || offer.name.toLowerCase().indexOf(this.offerSearch.toLowerCase()) !== -1) &&
+        (this.companySearch === '' || offer.username.toLowerCase().indexOf(this.companySearch.toLowerCase()) !== -1)) {
+        console.log("prosao proveru teksta")
+      }
+      else
+        continue;
+
+      if (this.isInternship) {
+        for (let item of offer.typeOfJob) {
+          if (item.item_text === "Internship") {
+            console.log("jeste praksa")
+            this.helper = true;
+          }
+        }
+        if (!this.helper)
+          continue
+      }
+
+      this.helper = false;
+      if (this.isJob) {
+        for (let item of offer.typeOfJob) {
+          if (item.item_text === "Job") {
+            console.log("jeste posao")
+            this.helper = true;
+          }
+        }
+        if (!this.helper)
+          continue
+      }
+
+      //Proslo je sve provere i mozemo da ga ubacimo
+      this.allOffers.push(offer)
+    }
+
+    this.checkOffersDate()
   }
 
 }
